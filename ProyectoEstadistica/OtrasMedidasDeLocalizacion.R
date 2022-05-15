833bf04f4029f2eb530a9fd2e1a6473d8fac399c
alexguecha <alexguecha@gmail.com>
2022-05-14 21:46:32 -0500

    mayo142022

---
+++ ProyectoEstadistica/OtrasMedidasDeLocalizacion.R
@@ -0,0 +1,45 @@
+# Media Geométrica
+
+
+#Una de sus principales ventajas es que es menos sensible a valores extremos (muy grandes o muy pequeños) 
+#que podrían alterar la media de una muestra estadística. Por el contrario, su principal desventaja es que 
+#no puede utilizarse con números negativos.
+
+#Función
+geometric<-function(x) exp(sum(log(x))/length(x))
+
+#Variable : Edad
+geometric(BDa2censo$Edad)
+#Media Geométrica de :   36.03653
+#Mean                    37.18176
+#Median                  35.00000
+
+#Variable : Tasa
+geometric(BDa2censo$Tasa)
+#Media Geométrica de :   0.103910
+#Mean                    0.104405
+#Median                  0.100000
+
+#Variable : Ingresos Mes
+geometric(BDa2censo$IngresosMes)
+#Media Geométrica de :   $5017014
+#Mean                    $7590004
+#Median                  $4878000
+
+#Variable : Patrimonio
+geometric(BDa2censo$Patrimonio)
+#Media Geométrica de :   $64676965
+#Mean                    $1.570082e+08
+#Median                  $1.000000e+08
+
+#Variable : MontoInversion
+geometric(BDa2censo$MontoInversion)
+#Media Geométrica de :      $611385.7
+#Mean                       $967367.6
+#Median                     $500000.0
+
+#Variable : CantidadInversiones
+geometric(BDa2censo$CantidadInversiones)
+#Media Geométrica de :   7.199434
+#Mean                    12.19810
+#Median                  8.000000
